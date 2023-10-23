1c398ee1a43e58ecf0aba7f139f1c20a484637d3
p3210090@aueb.gr <p3210090@aueb.gr>
2023-10-23 15:52:38 +0300

    changes

---
+++ assets/common.js
@@ -0,0 +1,4 @@
+// common.js
+document.getElementById("head").innerHTML = "<ul><li><a href='index.html'>Home</a></li><li><a href='about.html'>About</a></li><li><a href='contact.html'>Contact</a></li></ul>";
+document.getElementById("header").innerHTML = "<ul><li><a href='index.html'>Home</a></li><li><a href='about.html'>About</a></li><li><a href='contact.html'>Contact</a></li></ul>";
+document.getElementById("menu-button").innerHTML = "<ul><li><a href='index.html'>Home</a></li><li><a href='about.html'>About</a></li><li><a href='contact.html'>Contact</a></li></ul>";
